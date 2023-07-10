9181d59dc608f781c42e9f48c30b617913075c69
HuynhBao <huynhbao0626@gmail.com>
2023-07-11 01:42:56 +0700

    "Add ProductDAOImpl"

--- src/main/java/vn/aptech/componentmanagementapp/dao/ProductDAOImpl.java
+++ src/main/java/vn/aptech/componentmanagementapp/dao/ProductDAOImpl.java
@@ -3,10 +3,7 @@ package vn.aptech.componentmanagementapp.dao;
 import vn.aptech.componentmanagementapp.model.Product;
 import vn.aptech.componentmanagementapp.util.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,17 +45,56 @@ public class ProductDAOImpl implements ProductDAO{
     }
 
     @Override
-    public void add(Product entity) {
-
+    public void add(Product product) {
+        String query = "INSERT INTO products(id, product_code, name, price, minimum_price, stock_quantity, month_of_warranty, note, " +
+                "description, supplier_id, category_id) " + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try(PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setLong(1,product.getId());
+            statement.setString(2,product.getProductCode());
+            statement.setString(3,product.getName());
+            statement.setDouble(4,product.getPrice());
+            statement.setDouble(5,product.getMinimumPrice());
+            statement.setInt(6,product.getStockQuantity());
+            statement.setInt(7,product.getMonthOfWarranty());
+            statement.setString(8,product.getNote());
+            statement.setString(9,product.getDescription());
+            statement.setLong(10,product.getSupplierId());
+            statement.setLong(11,product.getCategoryId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void update(Product entity) {
-
+    public void update(Product product) {
+        String query = "UPDATE products SET product_code = ?, name = ?, price = ?, minimum_price = ?, stock_quantity = ?, month_of_warranty = ?, note = ?, " +
+                "description = ?, supplier_id = ?, category_id = ? WHERE id = ?";
+        try(PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1,product.getProductCode());
+            statement.setString(2,product.getName());
+            statement.setDouble(3,product.getPrice());
+            statement.setDouble(4,product.getMinimumPrice());
+            statement.setInt(5,product.getStockQuantity());
+            statement.setInt(6,product.getMonthOfWarranty());
+            statement.setString(7,product.getNote());
+            statement.setString(8,product.getDescription());
+            statement.setLong(9,product.getSupplierId());
+            statement.setLong(10,product.getCategoryId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void delete(long id) {
-
+    public void delete(long productId) {
+        String query = "DELETE FROM products WHERE id = ?";
+        try(PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setLong(1,productId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
