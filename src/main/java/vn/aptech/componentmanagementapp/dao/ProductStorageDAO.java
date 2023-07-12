725b779d07f1a5392bd5ddd9631761a26ce0901a
Tom Pro <dattran932017@gmail.com>
2023-07-12 21:44:48 +0700

    product storage

---
+++ src/main/java/vn/aptech/componentmanagementapp/dao/ProductStorageDAO.java
@@ -0,0 +1,7 @@
+package vn.aptech.componentmanagementapp.dao;
+
+import vn.aptech.componentmanagementapp.model.ProductStorage;
+
+public interface ProductStorageDAO extends BaseDAO<ProductStorage>{
+
+}
