abe9b2f47211e160362ccb67fbebf744bde08ac2
HuynhBao <huynhbao0626@gmail.com>
2023-07-13 03:52:20 +0700

    "Add unique + validate for email + phone"

--- sql script/postgresql.sql
+++ sql script/postgresql.sql
@@ -11,6 +11,9 @@ CREATE TABLE customers
 CREATE UNIQUE INDEX idx_unique_email
     ON customers (email)
     WHERE email IS NOT NULL AND email <> '';
+CREATE UNIQUE INDEX  ifx_unique_phone
+    ON customers (phone)
+WHERE phone IS NOT NULL AND phone <> '';
 
 CREATE TABLE departments
 (
